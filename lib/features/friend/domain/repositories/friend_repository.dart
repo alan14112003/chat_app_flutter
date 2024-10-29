beeced649a5b09061cc1bd766af50598897db27d
lethidanlien <lienltd.21it@vku.udn.vn>
2024-10-29 14:16:40 +0700

    feat: show friend get data from API

--- lib/features/friend/domain/repositories/friend_repository.dart
+++ lib/features/friend/domain/repositories/friend_repository.dart
@@ -1,7 +1,8 @@
 import 'package:chat_app_flutter/features/friend/domain/entities/friend.dart';
+import 'package:chat_app_flutter/features/friend/presentation/models/user.dart';
 
 abstract class FriendRepository {
-  Future<List<Friend>> getFriends();
+  Future<List<User>> getFriends();
   Future<void> addFriend(Friend friend);
   Future<void> removeFriend(int friendId);
 }
